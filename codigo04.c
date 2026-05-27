3b685103e54b74148a6735dfffffbf591e0d6a31
Jacqueline Paola Leyva Hernández <jacquelineleyvah@gmail.com>
2026-05-26 22:07:29 -0600

    Nowait en C
    Implement a demonstration of OpenMP's single nowait feature to optimize parallel execution.

---
+++ codigo04.c
@@ -0,0 +1,29 @@
+#Código 4: Optimización con nowait
+#Objetivo en la expo: Mostrar cómo logramos que la tarea la haga un único proceso cualquiera (como en single), pero eliminando la barrera para que los demás sigan trabajando de largo sin perder tiempo ocioso.
+
+#include <iostream>
+#include <omp.h>
+#include <unistd.h> // sleep()
+
+using namespace std;
+
+int main() {
+
+    cout << "=== DEMO 4: OMP SINGLE NOWAIT ===\n\n";
+
+    #pragma omp parallel num_threads(4)
+    {
+        int id = omp_get_thread_num();
+
+        #pragma omp single nowait
+        {
+            cout << "[Hilo " << id << "] ---> Ejecutando SINGLE con NOWAIT. Tardaré 3 segundos...\n";
+            sleep(3);
+            cout << "[Hilo " << id << "] ---> SINGLE con NOWAIT finalizado.\n";
+        }
+
+        cout << "[Hilo " << id << "] Avanzó sin esperar al single.\n";
+    }
+
+    return 0;
+}
